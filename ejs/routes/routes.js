bb0b92e97b7c522508d6806627808f3288c09095
suzukaotto <me@szk.kr>
2023-10-24 23:32:14 +0900

    commit

--- ejs/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req,res)=>{res.render("../views/index",{title:"Offline"})})
-router.get('/login', (req,res)=>{res.render("../views/login",{title:"Offline - Login"})})
-
-module.exports = router;
